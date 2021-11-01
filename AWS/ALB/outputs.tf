252e9974efa054f59520cca9beabb2af22f9dd45
kevin-travers <kevin.a.travers@gmail.com>
2021-11-01 13:16:45 -0400

    added outputs for alb resource

--- AWS/ALB/outputs.tf
+++ AWS/ALB/outputs.tf
@@ -1,12 +1,21 @@
-output "arn" {
-    value = aws_alb.alb.arn
-}
-output "zone_id" {
-    value = aws_alb.alb.zone_id
-}
 output "id" {
+    description = "The ARN of the load balancer (matches arn)."
     value = aws_alb.alb.id
 }
+output "arn" {
+    description = "The ARN of the load balancer (matches id)."
+    value = aws_alb.alb.arn
+}
+output "arn_suffix" {
+    description = " The ARN suffix for use with CloudWatch Metrics."
+    value = aws_alb.alb.arn_suffix
+}
 output "dns_name" {
+    description = "The DNS name of the load balancer."
     value = aws_alb.alb.dns_name
 }
+output "zone_id" {
+    description = "The canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
+    value = aws_alb.alb.zone_id
+}
+
